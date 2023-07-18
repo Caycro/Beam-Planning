215673ee939f20b172ebfcf2528fe874f0b2a71e
Caycrodon <121292533+Caycro@users.noreply.github.com>
2023-07-18 11:10:43 +0000

    modified:   Microsoft Form Link 	modified:   TODO 	modified:   app.py

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ import csv
 def LoadBlocks():
 
     lineCount=0
-    dataBase=[]
+    dataBase = dict
     #load the csv file into variable called database
     with open('BlockDetails.txt', mode= 'r') as blockCsvFile:
         csvReader = csv.DictReader(blockCsvFile,delimiter=",",dialect= "excel")
         for row in csvReader:
             if (lineCount>0):
-               dataBase.append(row)
+               dataBase+=row
             lineCount+=1
     return dataBase
 
 
 def LoadUsers():
     lineCount=0
-    dataBase=[]
+    dataBase=dict
     #load the csv file into variable called database
     with open('UserDetails.txt', mode= 'r') as blockCsvFile:
         csvReader = csv.DictReader(blockCsvFile,delimiter=",",dialect= "excel")
         for row in csvReader:
             if (lineCount>0):
-               dataBase.append(row)
+               dataBase
             lineCount+=1
     return dataBase
 
@@ -33,18 +33,8 @@ def last(myList):
 def main():
     blocks = LoadBlocks()
     users = LoadUsers()
-    details = []
-    for i in users:
-        details.append(i)
-        for j in blocks:
-            if (i[0] == j[0]):
-                last(details).append[blocks[1]]
-                last(details).append[blocks[2]]
-    for i in details:
+    total = users.update(blocks)
+    for i in total:
         print(i)
 
 main()
-
-
-
-
